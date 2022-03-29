d3d56d28b507d66124265747e999856fd9deb8da
danieljd <562674710@qq.com>
2022-03-29 17:25:19 +0800

    Revert "use commondata to show turnoverrate (#59)"
    This reverts commit 311baceeb8f0004694a182487122566b2cfe37b0.

--- client/component/new_alarm.tsx
+++ client/component/new_alarm.tsx
@@ -498,17 +498,15 @@ export const AlarmComponent = (props) => {
             }
           });
           const overRateArr = dateArr.map((i) => {
-            if (data?.[0]?.commonData?.find((d) => d.datestr === i)) {
-              return data?.[0]?.commonData?.find((d) => d.datestr === i)
-                .turnoverrate;
+            if (data.find((d) => d.datestr === i)) {
+              return data.find((d) => d.datestr === i).turnoverrate;
             } else {
               return '-';
             }
           });
           const priceArr = dateArr.map((i) => {
-            if (data?.[0]?.commonData?.find((d) => d.datestr === i)) {
-              return data?.[0]?.commonData?.find((d) => d.datestr === i)
-                .finalprice;
+            if (data.find((d) => d.datestr === i)) {
+              return data.find((d) => d.datestr === i).finalprice;
             } else {
               return '-';
             }
