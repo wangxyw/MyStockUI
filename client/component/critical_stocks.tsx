4582499e47415090926e34bb81002d4387f80af4
danieljd <562674710@qq.com>
2023-07-15 15:22:55 +0800

    D comments (#169)
    * comments
    
    * add sort by datestr
    
    * support one stock in DA
    
    * sorter
    
    * sorter
    
    * min vol
    
    * min vol
    
    * date
    
    * date
    
    * change date
    
    * fix issue
    
    * fix issue
    
    * fix issue
    
    * edit added
    
    * add loading
    
    * add date
    
    * add br
    
    * add min
    
    * fix defect
    
    * add viewd
    
    * add viewd
    
    * filter in list
    
    * plate
    
    * plate
    
    * new plate
    
    * jia quan
    
    * plate list
    
    * d_dr
    
    * d_dr
    
    * d_dr
    
    * d_dr
    
    * before days
    
    * k
    
    * k
    
    * k
    
    * 2 after add date
    
    * one date black
    
    * one stock before dates
    
    * one stock before dates
    
    * d date
    
    * date
    
    * d date
    
    * d date
    
    * d date
    
    * sort
    
    * add date improvement
    
    * date
    
    * add focus
    
    * add focus
    
    * add focus
    
    * add focus
    
    * second total
    
    * all da focus
    
    * all da focus
    
    * d add view
    
    * mgsy
    
    * d add view
    
    * d add focus
    
    * get to post
    
    * get to post
    
    * get to post
    
    * yesterday to today
    
    * da
    
    * da
    
    * da
    
    * da today alarm
    
    * update dates
    
    * revert total
    
    * total in order
    
    * total trade
    
    * remove plate condition1
    
    * export file
    
    * da today ready
    
    * today perpage 100
    
    * da plates
    
    * name
    
    * dedup plates
    
    * fouce more times
    
    * focus plate add datestr
    
    * critical stocks
    
    * todayprice - enddateprice
    
    * fix %
    
    * down
    
    * d
    
    * d
    
    * d
    
    * d
    
    * d
    
    * d
    
    * d
    
    * d
    
    * add k
    
    * add date
    
    * add date
    
    * d_bigger-to-smaller
    
    * fix date
    
    * add turnoverate
    
    * add maxmin price
    
    * add maxmin price
    
    * add maxmin price
    
    * add maxmin price
    
    * add maxmin price
    
    * add maxmin price
    
    * add maxmin price
    
    * add latest date
    
    * add merge options
    
    * ss
    
    * chart
    
    * add symbol
    
    ---------
    
    Co-authored-by: Jingdian Yue <jdyue@freewheel.tv>

--- client/component/critical_stocks.tsx
+++ client/component/critical_stocks.tsx
@@ -145,6 +145,7 @@ const MergeOptions = (data, downData) => {
       .split('|')
       .reduce((a, b) => (parseFloat(a) > parseFloat(b) ? a : b)),
     datestr: i.datestr,
+    haveLimit: i?.have_limit,
   }));
 
   const minT = orderedData?.map((i) => ({
@@ -152,12 +153,14 @@ const MergeOptions = (data, downData) => {
       ?.split('|')
       .reduce((a, b) => (parseFloat(a) > parseFloat(b) ? b : a)),
     datestr: i.datestr,
+    haveLimit: i?.have_limit,
   }));
   const maxDownT = orderedDownData?.map((i) => ({
     value: i?.turnoverrates_str
       .split('|')
       .reduce((a, b) => (parseFloat(a) > parseFloat(b) ? a : b)),
     datestr: i.datestr,
+    haveLimit: i?.have_limit,
   }));
 
   const minDownT = orderedDownData?.map((i) => ({
@@ -165,6 +168,7 @@ const MergeOptions = (data, downData) => {
       ?.split('|')
       .reduce((a, b) => (parseFloat(a) > parseFloat(b) ? b : a)),
     datestr: i.datestr,
+    haveLimit: i?.have_limit,
   }));
 
   const maxTValues = allData?.map((i) =>
@@ -172,19 +176,53 @@ const MergeOptions = (data, downData) => {
       ? maxT?.find((m) => m.datestr === i).value
       : '-'
   );
+  const maxTValuesMap = allData?.map((i) =>
+    maxT?.find((m) => m.datestr === i)
+      ? {
+          value: maxT?.find((m) => m.datestr === i).value,
+          haveLimit: maxT?.find((m) => m.datestr === i).haveLimit,
+        }
+      : '-'
+  );
+
   const minTValues = allData?.map((i) =>
     minT?.find((m) => m.datestr === i)
       ? minT?.find((m) => m.datestr === i).value
       : '-'
   );
+  const minTValuesMap = allData?.map((i) =>
+    minT?.find((m) => m.datestr === i)
+      ? {
+          value: minT?.find((m) => m.datestr === i).value,
+          haveLimit: minT?.find((m) => m.datestr === i).haveLimit,
+        }
+      : '-'
+  );
+
   const maxDownValues = allData?.map((i) =>
     maxDownT?.find((m) => m.datestr === i)
       ? maxDownT?.find((m) => m.datestr === i).value
       : '-'
   );
+  const maxDownValuesMap = allData?.map((i) =>
+    maxDownT?.find((m) => m.datestr === i)
+      ? {
+          value: maxDownT?.find((m) => m.datestr === i).value,
+          haveLimit: maxDownT?.find((m) => m.datestr === i).haveLimit,
+        }
+      : '-'
+  );
   const minDownValues = allData?.map((i) =>
     minDownT?.find((m) => m.datestr === i)
       ? minDownT?.find((m) => m.datestr === i).value
+      : '-'
+  );
+  const minDownValuesMap = allData?.map((i) =>
+    minDownT?.find((m) => m.datestr === i)
+      ? {
+          value: minDownT?.find((m) => m.datestr === i).value,
+          haveLimit: minDownT?.find((m) => m.datestr === i).haveLimit,
+        }
       : '-'
   );
 
@@ -241,13 +279,42 @@ const MergeOptions = (data, downData) => {
         name: 'MaxOverRate',
         type: 'line',
         data: maxTValues,
-        symbol: 'diamond',
-        symbolSize: 10,
+        symbol: (v, params) => {
+          var colorList;
+          if (maxTValuesMap[params.dataIndex]?.haveLimit == '1') {
+            colorList = 'arrow';
+          } else if (maxTValuesMap[params.dataIndex]?.haveLimit == '-1') {
+            colorList = 'circle';
+          } else if (maxTValuesMap[params.dataIndex]?.haveLimit == '2') {
+            colorList = 'pin';
+          } else {
+            colorList = 'diamond';
+          }
+
+          return colorList;
+        },
+        symbolSize: 15,
         itemStyle: {
           normal: {
             color: '#d50937',
+            // color: function (params) {
+            //   console.log('======', maxTValues, maxTValuesMap, params);
+            //   var colorList;
+            //   if (maxTValuesMap[params.dataIndex]?.haveLimit == '1') {
+            //     colorList = '#3c0202';
+            //   } else if (maxTValuesMap[params.dataIndex]?.haveLimit == '-1') {
+            //     colorList = '#023c1f';
+            //   } else if (maxTValuesMap[params.dataIndex]?.haveLimit == '2') {
+            //     colorList = '#3f09d5';
+            //   } else {
+            //     colorList = '#d50937';
+            //   }
+            //   console.log('colorList', colorList);
+            //   return colorList;
+            // },
           },
         },
+
         label: {
           position: 'top',
         },
@@ -255,9 +322,22 @@ const MergeOptions = (data, downData) => {
       {
         name: 'MinOverRate',
         type: 'line',
-        symbol: 'diamond',
+        // symbol: 'diamond',
         symbolSize: 10,
+        symbol: (v, params) => {
+          var colorList;
+          if (minTValuesMap[params.dataIndex]?.haveLimit == '1') {
+            colorList = 'arrow';
+          } else if (minTValuesMap[params.dataIndex]?.haveLimit == '-1') {
+            colorList = 'circle';
+          } else if (minTValuesMap[params.dataIndex]?.haveLimit == '2') {
+            colorList = 'pin';
+          } else {
+            colorList = 'diamond';
+          }
 
+          return colorList;
+        },
         itemStyle: {
           normal: {
             color: '#e07b7b',
@@ -281,7 +361,21 @@ const MergeOptions = (data, downData) => {
       {
         name: 'DownMaxOverRate',
         type: 'line',
-        symbol: 'diamond',
+        // symbol: 'diamond',
+        symbol: (v, params) => {
+          var colorList;
+          if (maxDownValuesMap[params.dataIndex]?.haveLimit == '1') {
+            colorList = 'arrow';
+          } else if (maxDownValuesMap[params.dataIndex]?.haveLimit == '-1') {
+            colorList = 'circle';
+          } else if (maxDownValuesMap[params.dataIndex]?.haveLimit == '2') {
+            colorList = 'pin';
+          } else {
+            colorList = 'diamond';
+          }
+
+          return colorList;
+        },
         symbolSize: 10,
 
         data: maxDownValues,
@@ -294,7 +388,21 @@ const MergeOptions = (data, downData) => {
       {
         name: 'DownMinOverRate',
         type: 'line',
-        symbol: 'diamond',
+        // symbol: 'diamond',
+        symbol: (v, params) => {
+          var colorList;
+          if (minDownValuesMap[params.dataIndex]?.haveLimit == '1') {
+            colorList = 'arrow';
+          } else if (minDownValuesMap[params.dataIndex]?.haveLimit == '-1') {
+            colorList = 'pin';
+          } else if (minDownValuesMap[params.dataIndex]?.haveLimit == '2') {
+            colorList = 'circle';
+          } else {
+            colorList = 'diamond';
+          }
+
+          return colorList;
+        },
         symbolSize: 10,
 
         data: minDownValues,
